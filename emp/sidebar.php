2b6247ea02373c1d7596e08083fa4a2db5650122
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-13 12:16:55 +0530

    chnage emp dashboard

--- emp/sidebar.php
+++ emp/sidebar.php
@@ -19,8 +19,8 @@
 <!-- Nav Item - Tables -->
 <li class="nav-item">
     <a class="nav-link" href="tables.php">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Tables</span></a>
+    <i class="fas fa-tasks"></i>
+        <span>Tasks</span></a>
 </li>
 
 </ul>
